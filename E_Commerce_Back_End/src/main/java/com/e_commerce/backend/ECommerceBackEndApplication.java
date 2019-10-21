2d73151304bca8e2fa23363aecc22f8d9c2fc55d
Md.Aslam Hossain <aslamecehstu43java@gmail.com>
2019-10-21 15:16:46 +0600

    Back-End created

---
+++ E_Commerce_Back_End/src/main/java/com/e_commerce/backend/ECommerceBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.e_commerce.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceBackEndApplication.class, args);
+	}
+
+}
---
+++ E_Commerce_Back_End/src/test/java/com/e_commerce/backend/ECommerceBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
